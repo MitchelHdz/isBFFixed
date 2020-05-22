52fc6f27f43ba0d2244121c0ab145b800eae5dd2
MitchelHdz <sebastian.hernandez@benandfrank.com>
2020-05-22 04:10:17 -0500

    Updates

---
+++ precache-manifest.5e36b8ce3168d8a0f6d1ee1abf382d4b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7ea911d2a41b37a21b0942943a26dfc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d49306156ac128e3b4c7",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b1adb96f39aa57088745",
+    "url": "/static/js/2.21479543.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.21479543.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d49306156ac128e3b4c7",
+    "url": "/static/js/main.be2a84ad.chunk.js"
+  },
+  {
+    "revision": "e065b33a4e4f9e0452f6",
+    "url": "/static/js/runtime-main.5c0df175.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
